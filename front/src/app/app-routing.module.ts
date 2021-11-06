fa3d1f5173ba322784fe99ad135841e7401c3fe2
Martin Choraine <martin.choraine@gmail.com>
2021-11-06 18:07:01 +0100

    add lazy loading

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from "./modules/home/home.component";
-import { CityComponent } from "./modules/city/city.component";
-import { CreateComponent } from "./modules/create/create.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./modules/home/home.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'create', component: CreateComponent},
-  { path: ':cityName', component: CityComponent},
+    {path: '', component: HomeComponent},
+    {path: 'create', loadChildren: () => import("./modules/create/create.module").then(m => m.CreateModule)},
+    {path: 'city', loadChildren: () => import("./modules/city/city.module").then(m => m.CityModule)},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,14 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './shared/components/navbar/navbar.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
-import {CityComponent} from './modules/city/city.component';
 import {HomeComponent} from './modules/home/home.component';
-import {LMapComponent} from './shared/components/lmap/lmap.component';
 import {HttpClientModule} from "@angular/common/http";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {DegreePipe} from './shared/pipes/degree.pipe';
-import {CreateComponent} from './modules/create/create.component';
-import {JeuConcoursComponent} from './shared/components/jeu-concours/jeu-concours.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
@@ -20,19 +14,13 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
         AppComponent,
         NavbarComponent,
         FooterComponent,
-        CityComponent,
-        HomeComponent,
-        LMapComponent,
-        DegreePipe,
-        CreateComponent
+        HomeComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
-        AppRoutingModule,
-        FormsModule,
-        ReactiveFormsModule
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
